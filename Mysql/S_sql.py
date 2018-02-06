a916c61ca1b6f36583392ee2f2d97fd0e620b5da
ROOT <934366858@qq.com>
2018-02-06 18:19:09 +0800

    S_autopjt-complete

---
+++ Mysql/S_sql.py
@@ -0,0 +1,6 @@
+import pymysql
+con = pymysql.connect(host='127.0.0.1',user='root', passwd = 'admin', db = 'mypydb')
+# con.query("create database mypydb")
+# con.query("create table mypydb.mytb(title ChAR(20) NOT NULL ,keywd CHAR (30))")
+con.query("insert into mytb(title, keywd, name) VALUES ('ccc','eee','你大爷')")
+# con.query("alter table mytb add COLUMN name VARCHAR (20)")
