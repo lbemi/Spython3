315bb4a05fe7fa5b7bc413b4826298646fcdbd27
wjl <934366858@qq.com>
2018-03-01 22:30:24 +0800

    change home

--- S_Django/blogproject/blog/urls.py
+++ S_Django/blogproject/blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('blog/',views.blog_title),
+    path('',views.blog_index),
+    path('blog/', views.blog_title),
+    path('blog/<article_id>/', views.blog_article)
 ]
--- S_Django/blogproject/blog/views.py
+++ S_Django/blogproject/blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from .models import BlogArticles
+from django.http import HttpResponse
+from django.template import loader
 
 def blog_title(request):
     blogs = BlogArticles.objects.all()
-    return render(request, 'blog/titles.html',{"blogs":blogs})
+    return render(request, 'blog/title.html',{"blogs":blogs})
+
+def blog_index(request):
+    return HttpResponse("首页测试！！！！")
+
+def blog_article(request, article_id):
+    article = BlogArticles.objects.get(id = article_id)
+    pub = article.publish
+    return render(request, 'blog/content.html', {'article':article, "publish":pub})
